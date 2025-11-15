d58b83bb89899539791964930aa22a170f082f4b
Aquarius Desktop <wwes20107@gmail.com>
2025-11-15 20:07:48 +0800

    try to add train imformation to log.txt

--- gym_test.py
+++ gym_test.py
@@ -20,32 +20,48 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 
-env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0', apply_api_compatibility=True, render_mode='rgb_array')
+import copy
 
+
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0', apply_api_compatibility=True, render_mode='rgb_array')
 env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
-done = True
 
-for step in range(1):
-    if done:
-        state = env.reset()
-        logger.debug(type(state))
-        for i in state: 
-            logger.debug(i)
-            logger.debug(type(i))
+done = False
+
+logger.debug(env.get_action_meanings())
+
+logger.debug(env.reset()[0])
+logger.debug(env.reset()[1])
+
+for episode_num in range(5):
+    obs, info = env.reset()
+    episode_reward = 0
+    step_count = 0
+
+    episode_over = False
+
+    with open ("log.txt", "a") as f:
+        print("-"*170, file=f)
+
+    # for every episode:
+    while not done:
+        # Sample a random action from the environment's action space
+        action = env.action_space.sample()  # Agent's policy can replace this for specific decision-making logic
+ 
+        # Step the environment using the sampled action
+        observation, reward, done, _, info = env.step(action)
+        last_obs = copy.deepcopy(observation)
+
+        state, reward, done, _, info = env.step(env.action_space.sample())
+        env.render()
 
         with open ("log.txt", "a") as f:
-            print("-" * 170, file=f)
+            print(f"step {step_count}: \n    reward: {reward}, \n    done: {done}, \n    info: {info}, \n    {time.strftime('%Y-%m-%d %H:%M:%S')}", file=f)
 
-    state, reward, done, _, info = env.step(env.action_space.sample())
-    env.render()
-    logger.debug(np.shape(state))
-    logger.debug(type(state))
-    with open ("log.txt", "a") as f:
-        print(f"step {step}: \n    reward: {reward}, \n    done: {done}, \n    info: {info}, \n    {time.strftime('%Y-%m-%d %H:%M:%S')}", file=f)
-    # logger.debug(f"step: {step}, state: {state}, reward: {reward}, done: {done}, trunc: {trunc}, info: {info}")
+        logger.debug(f"step: {step_count}, state: {state}, reward: {reward}, done: {done}, info: {info}")
 
-# env.close()
+env.close()
 
 
 def downscale_obs(obs: np.ndarray, new_size: Tuple=(240, 256), to_gray: bool=True) -> np.ndarray:
