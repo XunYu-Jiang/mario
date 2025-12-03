28f9f77387af0678d2a65fc05b7c5df498e19f1d
Aquarius Desktop <wwes20107@gmail.com>
2025-12-03 12:09:39 +0800

    add type hint

--- nnet_wrapper.py
+++ nnet_wrapper.py
@@ -1,15 +1,30 @@
+import torch
+from typing import Tuple
+
+import log_setting
+logger = log_setting.MyLogging.get_default_logger()
+
+
 class NNetWrapper():
-    def __init__(self, game, args):
+    def __init__(self, game, nnet: torch.nn.Module):
         raise NotImplementedError
 
-    def train(self, examples):
+    def train(self, train_data: torch.Tensor):
+        
         raise NotImplementedError
     
-    def predict(self, obs):
+    def predict(self, obs: torch.Tensor) -> torch.Tensor | Tuple:
         ...
     
-    def save_checkpoint(self, folder, filename):
+    def save_checkpoint(cls, folder, filename):
         ...
 
     def load_checkpoint(self, folder, filename):
         ...
+
+def main():
+    a = 10
+    logger.debug(a)
+
+if __name__ == "__main__":
+    main()
